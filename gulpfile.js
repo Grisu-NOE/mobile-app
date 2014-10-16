2d74e39156874d064662810e1033d177d9a76cc0
l-e-X <office@alex-gassner.at>
2014-10-16 23:39:42 +0200

    #1 Setup of Test engine

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,13 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var karma = require('gulp-karma');
 
 var paths = {
   sass: ['./scss/**/*.scss']
 };
 
-gulp.task('default', ['sass']);
+gulp.task('build', ['sass']);
 
 gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
@@ -25,8 +26,29 @@ gulp.task('sass', function(done) {
     .on('end', done);
 });
 
-gulp.task('watch', function() {
+gulp.task('autosass', function() {
   gulp.watch(paths.sass, ['sass']);
+});
+
+gulp.task('test', function() {
+  // Be sure to return the stream
+  // NOTE: Using the fake './foobar' so as to run the files
+  // listed in karma.conf.js INSTEAD of what was passed to
+  // gulp.src !
+  return gulp.src('./foobar')
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'run'
+    }))
+    .on('error', function(err) {
+      // Make sure failed tests cause gulp to exit non-zero
+      console.log(err);
+      this.emit('end'); //instead of erroring the stream, end it
+    });
+});
+
+gulp.task('autotest', function() {
+  return gulp.watch(['www/js/**/*.js', 'test/spec/*.js'], ['test']);
 });
 
 gulp.task('install', ['git-check'], function() {
