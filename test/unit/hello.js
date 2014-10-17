36608d064a51d6bc38d5a0dc0c799d0cec717176
l-e-X <office@alex-gassner.at>
2014-10-17 17:40:24 +0200

    #1 modified unit test to negative result to test CI

--- test/unit/hello.js
+++ test/unit/hello.js
@@ -1,5 +1,5 @@
 describe('Hello world', function() {
     it('says hello', function() {
-        expect("Hello world!").toEqual("Hello world!");
+        expect("ello world!").toEqual("Hello world!");
     });
 });
