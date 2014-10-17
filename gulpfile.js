a4cfc1adde16d9de86d8eeb413ddeaad87ecfbe2
l-e-X <office@alex-gassner.at>
2014-10-17 20:01:15 +0200

    #1 removed bower

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,8 @@
 var gulp = require('gulp');
-var gutil = require('gulp-util');
-var bower = require('bower');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
-var sh = require('shelljs');
 var karma = require('gulp-karma');
 
 var paths = {
@@ -43,24 +40,4 @@ gulp.task('test', function() {
 
 gulp.task('watch-test', function() {
   return gulp.watch(['www/js/**/*.js', 'test/unit/*.js'], ['test']);
-});
-
-gulp.task('install', ['git-check'], function() {
-  return bower.commands.install()
-    .on('log', function(data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-    });
-});
-
-gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-    );
-    process.exit(1);
-  }
-  done();
 });
