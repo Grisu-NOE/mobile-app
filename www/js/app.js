37f6a4bda8763713c7632d335cc0ac587a6f2a88
l-e-X <office@alex-gassner.at>
2014-10-18 18:41:01 +0200

    Added SVG map of Lower Austria. Some smaller changes...

--- www/js/app.js
+++ www/js/app.js
@@ -1,19 +1,20 @@
-// Ionic Starter App
+angular.module('app', ['ionic'])
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+    .config(['$ionicTabsConfig', function($ionicTabsConfig) {
+        // Override the Android platform default to add "tabs-striped" class to "ion-tabs" elements.
+        $ionicTabsConfig.type = '';
+    }])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar
+            // above the keyboard for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+
+            if (window.StatusBar) {
+                window.StatusBar.styleLightContent();
+            }
+        });
+    });
