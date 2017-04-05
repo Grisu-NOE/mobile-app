e22c06d0863bbeedf9f9f373f007f71fc4c382b1
ajgassner <hello@alex-gassner.at>
2017-04-05 22:18:53 +0200

    move fixme comment

--- src/components/grisu-refresher-content.component.ts
+++ src/components/grisu-refresher-content.component.ts
@@ -1,5 +1,8 @@
-import {Component} from "@angular/core";
+import { Component } from "@angular/core";
 
+/**
+ * FIXME: Introduce an own refresher component (not only the content) when https://github.com/driftyco/ionic/issues/10540 gets resolved.
+ */
 @Component({
 	selector: "grisu-refresher-content",
 	template: `<ion-refresher-content pullingText="Aktualisieren" refreshingText="Lade Daten..."></ion-refresher-content>`
