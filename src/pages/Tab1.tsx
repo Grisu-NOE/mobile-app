930522148ed0fc4e62f7eec5bc89df3b70f8f512
Alex Gassner <hello@agsolutions.at>
2019-12-29 20:32:34 +0100

    playing with react

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,71 +1,66 @@
 import {
-  IonCard,
-  IonCardContent,
-  IonCardHeader,
-  IonCardSubtitle,
-  IonCardTitle,
-  IonContent,
-  IonHeader,
-  IonIcon,
-  IonItem,
-  IonLabel,
-  IonList,
-  IonListHeader,
-  IonPage,
-  IonTitle,
-  IonToolbar
+	IonButton, IonButtons,
+	IonCol,
+	IonContent,
+	IonGrid,
+	IonHeader,
+	IonIcon,
+	IonItem,
+	IonList,
+	IonPage,
+	IonRow,
+	IonTitle,
+	IonToolbar
 } from '@ionic/react';
-import { book, build, colorFill, grid } from 'ionicons/icons';
+import {star, informationCircle, mail, settings} from 'ionicons/icons';
 import React from 'react';
 import './Tab1.css';
+import LowerAustriaMap from "../components/LowerAustriaMap";
 
 const Tab1: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab One</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <IonCard className="welcome-card">
-          <img src="/assets/shapes.svg" alt="" />
-          <IonCardHeader>
-            <IonCardSubtitle>Get Started</IonCardSubtitle>
-            <IonCardTitle>Welcome to Ionic</IonCardTitle>
-          </IonCardHeader>
-          <IonCardContent>
-            <p>
-              Now that your app has been created, you'll want to start building out features and
-              components. Check out some of the resources below for next steps.
-            </p>
-          </IonCardContent>
-        </IonCard>
+	return (
+		<IonPage>
+			<IonHeader>
+				<IonToolbar>
+					<IonButtons slot="start">
+						<IonButton>
+							<IonIcon slot="icon-only" icon={informationCircle}/>
+						</IonButton>
+					</IonButtons>
+					<IonTitle>Grisu NÖ</IonTitle>
+					<IonButtons slot="end">
+						<IonButton>
+							<IonIcon slot="icon-only" icon={mail}/>
+						</IonButton>
+						<IonButton>
+							<IonIcon slot="icon-only" icon={settings}/>
+						</IonButton>
+					</IonButtons>
+				</IonToolbar>
+			</IonHeader>
+			<IonContent>
+				<IonGrid>
+					<IonRow>
+						<IonCol size-md align-self-center>
+							<IonList inset>
+								<IonItem no-padding>Aktuelle Einsätze: 1</IonItem>
+								<IonItem no-padding>Ausgerückte Feuerwehren: 2</IonItem>
+								<IonItem no-padding>Aktive Bezirke: 3</IonItem>
+							</IonList>
 
-        <IonList lines="none">
-          <IonListHeader>
-            <IonLabel>Resources</IonLabel>
-          </IonListHeader>
-          <IonItem href="https://ionicframework.com/docs/" target="_blank">
-            <IonIcon slot="start" color="medium" icon={book} />
-            <IonLabel>Ionic Documentation</IonLabel>
-          </IonItem>
-          <IonItem href="https://ionicframework.com/docs/building/scaffolding" target="_blank">
-            <IonIcon slot="start" color="medium" icon={build} />
-            <IonLabel>Scaffold Out Your App</IonLabel>
-          </IonItem>
-          <IonItem href="https://ionicframework.com/docs/layout/structure" target="_blank">
-            <IonIcon slot="start" color="medium" icon={grid} />
-            <IonLabel>Change Your App Layout</IonLabel>
-          </IonItem>
-          <IonItem href="https://ionicframework.com/docs/theming/basics" target="_blank">
-            <IonIcon slot="start" color="medium" icon={colorFill} />
-            <IonLabel>Theme Your App</IonLabel>
-          </IonItem>
-        </IonList>
-      </IonContent>
-    </IonPage>
-  );
+							<IonButton expand="full" color="dark">
+								<IonIcon slot="start" icon={star}/>
+								Meine Einsatzhistorie
+							</IonButton>
+						</IonCol>
+						<IonCol size-md align-self-center>
+							<LowerAustriaMap onClick={district => console.log(district)}/>
+						</IonCol>
+					</IonRow>
+				</IonGrid>
+			</IonContent>
+		</IonPage>
+	);
 };
 
 export default Tab1;
